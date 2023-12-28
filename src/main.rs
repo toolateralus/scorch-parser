f9c059d5936fa7e298a5a19f67110520a621d8b4
toolateralus <joshnjoshgaming@gmail.com>
2023-12-28 11:36:45 -0500

    added a basic test, restructuer

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,35 @@ pub mod ast;
 pub mod lexer;
 pub mod parser;
 
-
-fn main() {
+pub fn main() {
     
+}
+
+
+#[cfg(test)]
+mod tests {
+    use lexer::{create_tokenizer, TokenProcessor};
+
+    use super::*;
+
+    #[test]
+    fn test_main() {
+        // todo: make testing much much better in this project.
+        // tests for both tokenizer and parser, individual rules & actual unit size tests.
+        let input = "
+        x : Int = 1 + (2 * 3)
+        funcy : Fn(x : Int) -> Bool {
+            break 250 * x
+        }
+        [funcy(2 * x)].println()
+        ";
+        
+        let mut tokenizer = create_tokenizer();   
+        
+        tokenizer.tokenize(input);
+        
+        let ast_root = parser::parse_program(&tokenizer.tokens);
+        
+        dbg!(&ast_root);
+    }
 }
